65ddb93d8d25080a4558c22181d2afe40b86db28
Teo Ming Yi <mingyiteo@u.nus.edu>
2016-09-08 22:32:51 +0800

    Add test for when names are the same

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 public class NameTest {
 
     @Test
-    public void test() {
-	fail("Not yet implemented");
+    public void names_areEqual() throws IllegalValueException {
+	Name personOne = new Name("John Doe");
+	Name personTwo = new Name("John Doe");
+	assertEquals(true, personOne.isSimilar(personTwo));
     }
 
 }
