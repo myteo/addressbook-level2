ac0d491ac78f1851aa01f61243e457f613c7ce68
Teo Ming Yi <mingyiteo@u.nus.edu>
2016-09-08 23:15:20 +0800

    Write code to pass test for when other name is superset/subset

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Represents a Person's name in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ * Represents a Person's name in the address book. Guarantees: immutable; is
+ * valid as declared in {@link #isValidName(String)}
  */
 public class Name {
 
@@ -21,45 +21,47 @@ public class Name {
     /**
      * Validates given name.
      *
-     * @throws IllegalValueException if given name string is invalid.
+     * @throws IllegalValueException
+     *             if given name string is invalid.
      */
     public Name(String name) throws IllegalValueException {
-        name = name.trim();
-        if (!isValidName(name)) {
-            throw new IllegalValueException(MESSAGE_NAME_CONSTRAINTS);
-        }
-        this.fullName = name;
+	name = name.trim();
+	if (!isValidName(name)) {
+	    throw new IllegalValueException(MESSAGE_NAME_CONSTRAINTS);
+	}
+	this.fullName = name;
     }
 
     /**
      * Returns true if a given string is a valid person name.
      */
     public static boolean isValidName(String test) {
-        return test.matches(NAME_VALIDATION_REGEX);
+	return test.matches(NAME_VALIDATION_REGEX);
     }
 
     /**
      * Retrieves a listing of every word in the name, in order.
      */
     public List<String> getWordsInName() {
-        return Arrays.asList(fullName.split("\\s+"));
+	return Arrays.asList(fullName.split("\\s+"));
     }
 
     @Override
     public String toString() {
-        return fullName;
+	return fullName;
     }
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
-                || (other instanceof Name // instanceof handles nulls
-                && this.fullName.equals(((Name) other).fullName)); // state check
+	return other == this // short circuit if same object
+		|| (other instanceof Name // instanceof handles nulls
+			&& this.fullName.equals(((Name) other).fullName)); // state
+									   // check
     }
 
     @Override
     public int hashCode() {
-        return fullName.hashCode();
+	return fullName.hashCode();
     }
 
     public boolean isSimilar(Name other) {
@@ -69,10 +71,15 @@ public class Name {
 	else if(this.fullName.equals(other.fullName)){
 	    return true;
 	}
-	if(this.fullName.equalsIgnoreCase(other.fullName)){
+	else if(this.fullName.equalsIgnoreCase(other.fullName)){
 	    return true;
 	}
-	boolean isSame = false;
+	else if(this.fullName.contains(other.fullName) || 
+		other.fullName.contains(this.fullName)){
+	    return true;
+	}
+	else {
+	    boolean isSame = false;
 	String[] a = this.fullName.split(" ");
 	String[] b = this.fullName.split(" ");
 	List<String> ab = Arrays.asList(a);
@@ -89,5 +96,5 @@ public class Name {
 	return isSame;
 	
     }
-
+    }
 }
